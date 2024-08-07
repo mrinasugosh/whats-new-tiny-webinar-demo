aa772418535d277eaae948a85d1c6f3489b0a73f
Mrina Sugosh <mrina.sugosh@gmail.com>
2024-08-07 10:11:37 -0400

    init: added init value to main

--- src/components/TinyEditor.jsx
+++ src/components/TinyEditor.jsx
@@ -12,7 +12,7 @@ export default function TinyEditor() {
         plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount linkchecker',
         toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
       }}
-      initialValue="Welcome to TinyMCE!"
+      initialValue='<p dir="ltr">Hey {{Student.FirstName}}!</p><br/><p dir="ltr"> I received your question regarding the homework problem. This is what you need to solve this:</p><br/><p dir="ltr">If you need further clarification, please refer to the lecture notes from last week, or feel free to ask me questions on the steps you are getting stuck at.</p><br/><p dir="ltr">Regards,</p><p dir="ltr">{{Professor.FirstName}}</p>'
     />
   );
 }
